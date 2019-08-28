7855d4b9980ca373edb615f7b0bb5c0011abd50a
Owen Atamari <32944160+theowex@users.noreply.github.com>
2019-08-28 14:08:39 -0500

    Update ejercicio6.cpp

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -4,3 +4,19 @@
  Y encuentra el mayor valor: 30
 
  */
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+  int num;
+  vector <int> lista;
+  for(int i=0;i<3;i++){
+    cin>>num;
+    lista.push_back(num);
+  }
+  sort(lista.begin(), lista.end());
+  cout<<lista[2];
+}
