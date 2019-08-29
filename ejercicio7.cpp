132f2019cc5f1449dbb405558e207395105e0663
Owen Atamari <32944160+theowex@users.noreply.github.com>
2019-08-28 21:24:22 -0500

    Update ejercicio7.cpp

--- ejercicio7.cpp
+++ ejercicio7.cpp
@@ -1,26 +1,22 @@
 /*
  El usuario puede escoger de 5 opciones (de 1 a 5). Entonces pregunta por dos
  valores enteros para el calculo
-
  MENU:
  1.- suma
  2.- resta
  3.- multiplicar
  4.- dividir
  5.- modulo
-
  Ingresa una opcion: 1
  Ingresa los dos numeros: 5 96
  Resultado: 111
-
  Continuar? y
-
  // Si el usuario ingresa 'y' a la pregunta Continuar? el usuario puede escoger otra opcion.
-
  */
 
 #include <iostream>
 #include <stdexcept> //Validar en la division por 0
+
 using std::cout;
 using std::cin;
 using std::invalid_argument;
@@ -32,18 +28,61 @@ int multiplicar(int a, int b);
 float dividir(int a, int b);
 int modulo(int a, int b);
 
+int sumar(int a, int b) {
+    return a + b;
+}
+int restar(int a, int b) {
+    return a - b;
+}
+int multiplicar(int a, int b) {
+    return a * b;
+}
+float dividir(int a, int b) {
+    return a / b;
+}
+int modulo(int a, int b) {
+    return a % b;
+}
 
 void mostrarMenu() {
     cout<<"***************************************************\n";
     cout<<"                     MENU                          \n";
     cout<<"***************************************************\n";
-    cout<<"  1.- Add\n";
+    cout<<"  1.- Suma\n";
     cout<<"  2.- Resta\n";
     cout<<"  3.- Multiplicar\n";
     cout<<"  4.- Dividir\n";
-    cout<<"  6.- Modulo\n";
+    cout<<"  5.- Modulo\n";
 }
 
 int main() {
-    return 0;
+    int x, y, p;
+    char c;
+    mostrarMenu();
+    do{
+        cout<<"Ingresa una opcion: ";
+        cin>>p;
+        cout<<"Ingresa los dos numeros: ";
+        cin>>x>>y;
+        switch(p) {
+            case 1:
+                cout<<sumar(x, y)<<"\n";
+                break;
+            case 2:
+                cout<<restar(x, y)<<"\n";
+                break;
+            case 3:
+                cout<<multiplicar(x, y)<<"\n";
+                break;
+            case 4:
+                cout<<dividir(x, y)<<"\n";
+                break;
+            case 5:
+                cout<<modulo(x, y)<<"\n";
+                break;
+            }
+        cout<<"Continuar? ";
+        cin>>c;
+    }while(c == 'y');
+
 }
