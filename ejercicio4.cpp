38efc2da386aca1ae7737188d536c13b619bf224
Owen Atamari <32944160+theowex@users.noreply.github.com>
2019-08-29 12:13:32 -0500

    Update ejercicio4.cpp

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -1,20 +1,26 @@
 /*
 Escribir un programa que imprima lo siguiente:
-
 12*****
 123****
 1234***
 12345**
 123456*
 1234567
-
 */
 
 #include <iostream>
+
+
 using std::cout;
 
 int main() {
-
-
+    int i, k;
+    for(int j = 2; j <= 7; j++){
+        for(i = 1; i <= j; i++)
+            cout<<i;
+        for(k = 0; k <= 7-i; k++)
+            cout<<'*';
+        cout<<"\n";
+    }
     return 0;
 }
