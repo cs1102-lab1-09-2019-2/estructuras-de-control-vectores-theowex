7aeaa9c9d5f32d302f86a4cfaa16e6080404639b
Owen Atamari <32944160+theowex@users.noreply.github.com>
2019-08-30 18:07:31 -0500

    Update ejercicio5.cpp

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,19 +1,15 @@
 /*
  Dado una funcion cuadratica, donde los parametros a, b, y c son leidos por la consola.
  Calcular las soluciones basado en las siguientes reglas:
-
  Leer del usuario a, b, y c separados por espacio:
  cin>>a>>b>>c;
-
  1.- Si a y b son cero, entonces: No tiene solucion: a==0, b ==0..
  2.- Si a es cero, entonces: Tiene una sola solucion: (-c/b)
  3.- Si b^2 -4ac es negativo, entonces: No tiene solucion: delta < 0..
  4.- En el resto de los casos tiene dos soluciones y es calculado con la siguiente
  formula:
-
  x1 = -b + ((b^2 - 4ac)^(1/2))/2a
  x2 = -b - ((b^2 - 4ac)^(1/2))/2a
-
  La ecuacion tiene dos soluciones: x1 = x1, x2 = x2
 */
 
@@ -24,5 +20,20 @@ using namespace std;
 
 int main() {
     float a, b, c, delta, x1, x2;
+    cin>>a>>b>>c;
+    if(a == 0 && b == 0){
+        cout<<"No tiene solucion: a==0, b ==0..";
+    }
+    else if(a == 0){
+        cout<<"Tiene una sola solucion: "<<-c/b;
+    }
+    else if((pow(b, 2) - 4*a*c)/abs((pow(b, 2) - 4*a*c)) == -1){
+        cout<<"No tiene solucion: delta < 0..";
+    }
+    else{
+        x1 = -b + (sqrt(pow(b,2) - 4*a*c))/(2*a);
+        x2 = -b - (sqrt(pow(b,2) - 4*a*c))/(2*a);
+        cout<<"La ecuacion tiene dos soluciones: x1 = "<<x1<<", x2 = "<<x2;
+    }
 
 }
